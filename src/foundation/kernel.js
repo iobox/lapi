14b0b8fff33851db03d534f2f2c933cc69e721db
Long Do <long.do@audiencemedia.com>
2017-01-03 15:02:18 +0700

    Remove has, get and set in Kernel

--- src/foundation/kernel.js
+++ src/foundation/kernel.js
@@ -31,35 +31,4 @@ export default class Kernel {
       throw new Error('[Foundation/Kernel#setContainer] An instance of Container is required')
     }
   }
-
-  /**
-   * Determine if abstract is registered
-   * @param {string} abstract
-   * @returns {boolean}
-   */
-  has(abstract) {
-    return this.getContainer().has(abstract)
-  }
-
-  /**
-   * Get a concrete of specific abstract
-   * @param {string} abstract
-   * @returns {*}
-   */
-  get(abstract) {
-    if (!this.has(abstract)) {
-      throw new Error('[Foundation/Kernel#get] ' + abstract + ' is not registered')
-    }
-
-    return this.getContainer().get(abstract)
-  }
-
-  /**
-   * Set a concrete to be an implementation of abstract
-   * @param {string} abstract
-   * @param {?*} concrete
-   */
-  set(abstract, concrete) {
-    this.getContainer().set(abstract, concrete)
-  }
 }
